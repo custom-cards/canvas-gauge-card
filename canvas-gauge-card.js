0b583df6514ab10d797fc764d2ed251555cda2ab
helto4real <tomas.hellstrom@yahoo.se>
2018-07-27 09:57:26 +0200

    removed the local testing

--- canvas-gauge-card.js
+++ canvas-gauge-card.js
@@ -41,9 +41,9 @@ import "./gauge.min.js"
         - type: custom:canvas-gauge-card
             entity: sensor.temp_outside
             gauge:
-            type: "linear-gauge"
-            width: 120
-            height: 400
+                type: "linear-gauge"
+                width: 120
+                height: 400
     - Use the javascript properties withouth the ',' to configure properties under 'gauge'
     - Use name property to show the name att bottom of the card
     - See http://sss for docs
@@ -52,7 +52,6 @@ class CanvasGaugeCard extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
-        this.__initTests();
     }
     /**
      * Renders the card 
@@ -230,64 +229,7 @@ class CanvasGaugeCard extends HTMLElement {
     getCardSize() {
         return 2;
     }
-
-    /** 
-     * Init testdata
-     * I use this in my development environment to make a very simple mock of config/hass objects 
-    */
-    __initTests() {
-        var test_config =
-        {
-            entity: 'sensor.temp',
-            name: 'Test',
-            card_height: 190,
-            card_width: undefined,
-            card_top: undefined,
-            card_left: undefined,
-            gauge: {}
-        };
-
-        test_config.gauge['type'] = 'radial-gauge';
-        test_config.gauge['title'] = 'Speed';
-        test_config.gauge['width'] = 300;
-        test_config.gauge['height'] = 300;
-        test_config.gauge['units'] = 'Km/h';
-        test_config.gauge['minValue'] = 0;
-        test_config.gauge['maxValue'] = 220;
-        test_config.gauge['startAngle'] = 90;
-        test_config.gauge['ticksAngle'] = 180;
-        test_config.gauge['valueBox'] = false;
-        test_config.gauge['majorTicks'] = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220];
-        test_config.gauge['minorTicks'] = 2;
-        test_config.gauge['strokeTicks'] = true;
-        test_config.gauge['highlights'] = [
-            {
-                "from": 160,
-                "to": 220,
-                "color": "rgba(200, 50, 50, .75)"
-            }
-        ];
-        test_config.gauge['colorPlate'] = '#fff';
-        //       test_config.gauge['borderShadowWidth'] = 0;
-        //       test_config.gauge['borderOuterWidth'] = 0;
-        //       test_config.gauge['borderMiddleWidth'] = 0;
-        //       test_config.gauge['borderInnerWidth'] = 0;
-        test_config.gauge['borders'] = false;
-        test_config.gauge['needleType'] = 'arrow';
-        test_config.gauge['needleWidth'] = 2;
-        test_config.gauge['needleCircleSize'] = 7;
-        test_config.gauge['needleCircleOuter'] = true;
-        test_config.gauge['needleCircleInner'] = false;
-        test_config.gauge['animationDuration'] = 1500;
-        test_config.gauge['animationRule'] = 'linear';
-        test_config.gauge['value'] = '50';
-
-        var test_hass = { states: [] };
-        test_hass.states[test_config.entity] = { state: "15" };
-        this.setConfig(test_config);
-
-        this.hass = test_hass;
-    }
+  
 }
 
 window.customElements.define('canvas-gauge-card', CanvasGaugeCard);
